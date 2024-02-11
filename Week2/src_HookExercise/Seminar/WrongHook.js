e8f0abe6090a122257b66d58176677cce0c9693d
minzziPark <minzzi0068@gmail.com>
2024-02-11 17:52:20 +0900

    add : 2차 자료 useEffect 추가

--- Week2/src_HookExercise/Seminar/WrongHook.js
+++ Week2/src_HookExercise/Seminar/WrongHook.js
@@ -1,14 +1,14 @@
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 
-function WrongHook() {
-	const [name, setName] = useState('PARD');
-	
-	if(name !== ''){ // 조건문 안 useEffect Hook 삽입 -> 잘못된 hook 선언
-		useEffect(() => {
-			...
-		});
-	}
-  return <></>;
-}
+// function WrongHook() {
+// 	const [name, setName] = useState('PARD');
 
-export default WrongHook;
+// 	if(name !== ''){ // 조건문 안 useEffect Hook 삽입 -> 잘못된 hook 선언
+// 		useEffect(() => {
+// 			...
+// 		});
+// 	}
+//   return <></>;
+// }
+
+// export default WrongHook;
